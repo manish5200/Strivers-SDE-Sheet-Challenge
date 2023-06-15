4ed1ec5b49f6ea4b378ee505ca156dcbd2989195
Manish Kumar Singh <106795001+manish5200@users.noreply.github.com>
2023-06-15 22:09:51 +0530

    Is Bipartite
    #StriverSDEChallenge

--- IsBipartite.cpp
+++ IsBipartite.cpp
@@ -1,4 +1,39 @@
-//DFS -->
+//DFS1-->
+#include<bits/stdc++.h>
+bool dfs(int start,int col,int color[],vector<int>adj[]){
+	     color[start]=col;
+	    for(auto it:adj[start]){
+			if(color[it]==-1){
+				if(dfs(it,!col,color,adj)==false){
+					return false;
+				}
+			}else if(color[it]==color[start]){
+				return false;
+			}
+		}
+		return true;
+}
+bool isGraphBirpatite(vector<vector<int>> &edges) {
+	    int n =edges.size();
+			vector<int>adj[n+1];
+			for(int i=0;i<n;++i){
+				for(int j=0;j<n;++j){
+                    if(edges[i][j]){
+						adj[i].push_back(j);
+						adj[j].push_back(i);
+					}
+				}
+			}
+            int color[n+1];
+            memset(color,-1,sizeof color);
+			for(int i=0;i<n;++i){
+				if(color[i]==-1){
+					if(dfs(i,0,color,adj)==false)return false;
+				}
+			}
+			return true;
+}
+//DFS2 --->
 #include<bits/stdc++.h>
 bool dfs(int start,int color[],vector<int>adj[]){
 	    for(auto it:adj[start]){
