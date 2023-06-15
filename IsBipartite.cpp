aadf8a7adf913a9b7ce3b4d587ee2577d6f3b15f
Manish Kumar Singh <106795001+manish5200@users.noreply.github.com>
2023-06-15 22:06:00 +0530

    Is Bipartite
    #StriverSDEChallenege

---
+++ IsBipartite.cpp
@@ -0,0 +1,78 @@
+//DFS -->
+#include<bits/stdc++.h>
+bool dfs(int start,int color[],vector<int>adj[]){
+	    for(auto it:adj[start]){
+			if(color[it]==-1){
+				color[it]=!color[start];
+				if(dfs(it,color,adj)==false){
+					return false;
+				}
+			}else if(color[it]==color[start]){
+				return false;
+			}
+		}
+		return true;
+}
+bool isGraphBirpatite(vector<vector<int>> &edges) {
+	    int n =edges.size();
+			vector<int>adj[n+1];
+			for(int i=0;i<n;++i){
+				for(int j=0;j<n;++j){
+                    if(edges[i][j]){
+						adj[i].push_back(j);
+						adj[j].push_back(i);
+					}
+				}
+			}
+            int color[n+1];
+            memset(color,-1,sizeof color);
+			for(int i=0;i<n;++i){
+				if(color[i]==-1){
+					color[i]=1;
+					if(dfs(i,color,adj)==false)return false;
+				}
+			}
+			return true;
+}
+
+//<----- BFS ---->
+#include<bits/stdc++.h>
+bool check(int start,int color[],vector<int>adj[]){
+	  queue<int>q;
+	  color[start]=1;
+	  q.push(start);
+	  while(!q.empty()){
+		  int node=q.front();
+		  q.pop();
+		for(auto it:adj[node]){
+			if(color[it]==-1){
+				color[it]= !color[node];
+				q.push(it);
+			}
+			else if(color[it]==color[node]){
+				return false;
+			}
+		}
+	  }
+	  return true;
+}
+bool isGraphBirpatite(vector<vector<int>> &edges) {
+	    int n =edges.size();
+			vector<int>adj[n+1];
+			for(int i=0;i<n;++i){
+				for(int j=0;j<n;++j){
+                    if(edges[i][j]){
+						adj[i].push_back(j);
+						adj[j].push_back(i);
+					}
+				}
+			}
+            int color[n+1];
+            memset(color,-1,sizeof color);
+			for(int i=0;i<n;++i){
+				if(color[i]==-1){
+					if(check(i,color,adj)==false)return false;
+				}
+			}
+			return true;
+}
