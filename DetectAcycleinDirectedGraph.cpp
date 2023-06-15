22b63d9e55b09f774fedb782f762ab839e84bf41
Manish Kumar Singh <106795001+manish5200@users.noreply.github.com>
2023-06-15 22:40:28 +0530

    Detect A cycle in Directed Graph(DFS & BFS Both)
    #StriverSDEChallenge

--- DetectAcycleinDirectedGraph.cpp
+++ DetectAcycleinDirectedGraph.cpp
@@ -1,4 +1,34 @@
-
+//DFS --> 
+bool dfs(int start,int vis[],int pathVis[],vector<int>adj[]){
+     vis[start]=1;
+     pathVis[start]=1;
+     for(auto it:adj[start]){
+           if(!vis[it]){
+             if(dfs(it,vis,pathVis,adj)==true){
+               return true;
+             }
+           }
+           else if(pathVis[it]){
+                return true;
+           }
+     }
+     pathVis[start]=0;
+     return false;
+}
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+          vector<int>adj[n+1];
+         for(auto it:edges){
+          adj[it.first].push_back(it.second);
+         }
+         int vis[n+1]={0};
+         int pathVis[n+1]={0};
+        for(int i=0;i<n;++i){
+          if(!vis[i]){
+             if(dfs(i,vis,pathVis,adj)==true)return true;
+          }
+        }
+        return false;
+} 
 // <------ BFS -> Kahn's Algorithm ---->
 #include<bits/stdc++.h>
 int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
