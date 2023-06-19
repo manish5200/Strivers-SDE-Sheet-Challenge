51a2605c8395ace45731e22a693b77cb263ba8b7
Manish Kumar Singh <106795001+manish5200@users.noreply.github.com>
2023-06-19 17:22:50 +0530

    Search In Rotated Sorted Array
    #StriverSDEChallenge

--- SearchInRotatedSortedArray.cpp
+++ SearchInRotatedSortedArray.cpp
@@ -1,3 +1,4 @@
+//Itterative Solution
 int search(int* arr, int n, int key) {
     int l=0;
     int h = n-1;
@@ -20,4 +21,45 @@ int search(int* arr, int n, int key) {
         }
     }
     return -1;
+}
+//Recursive Solution 
+
+int getPivot(int* arr ,int n){
+       int l=0;
+       int h=n-1;
+       while(l<h){
+            int mid=l+(h-l)/2;
+          if(arr[mid]>=arr[0]){
+                l=mid+1;
+          }else{
+               h=mid;
+          }
+       }
+       return l;
+}
+int BinarySearch(int* arr ,int s , int e ,int k){
+           int l=s;
+           int h=e;
+        while(l<=h){
+             int mid=l+(h-l)/2;
+             if(arr[mid]==k){
+                  return mid;
+             }else if(arr[mid]>k){
+                   h=mid-1;
+             }else{
+                   l=mid+1;
+             }
+        }
+        return -1;
+}
+int search(int* arr, int n, int key) {
+         int s=0;
+         int e=n-1;
+         int mid=s+(e-s)/2;
+      int pivot=getPivot(arr, n);
+      if(arr[pivot]<=key && key<=arr[e]){
+             return BinarySearch(arr,pivot, e,key);
+      }else{
+            return BinarySearch(arr,0,pivot-1,key);
+      }
 }
